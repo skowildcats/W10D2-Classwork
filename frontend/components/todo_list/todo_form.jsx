3d596d31afea683191a69f180b6ba23d0ee0f322
Jon Chen <jonathan.chen.6799@gmail.com>
2021-06-23 14:28:30 -0500

    Added reset form functionality

--- frontend/components/todo_list/todo_form.jsx
+++ frontend/components/todo_list/todo_form.jsx
@@ -7,7 +7,7 @@ export default class TodoForm extends React.Component {
     this.state = {
       title: "",
       body: "",
-      done: true
+      done: null
     }
   };
 
@@ -27,22 +27,25 @@ export default class TodoForm extends React.Component {
     e.preventDefault();
     const todo = {...this.state, id:uniqueId()}
     this.props.receiveTodo(todo);
+    this.setState({title: "",
+      body: "",
+      done: null})
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)}>
         <label htmlFor="title">title</label>
-        <input id="title" type="text" onChange={this.handleTitle.bind(this)}/><br/>
+        <input id="title" type="text" value={this.state.title} onChange={this.handleTitle.bind(this)}/><br/>
 
         <label htmlFor="body">body</label>
-        <input id="body" type="text" onChange={this.handleBody.bind(this)}/><br/>
+        <input id="body" type="text" value={this.state.body} onChange={this.handleBody.bind(this)}/><br/>
 
         <div>done:</div>
         <label htmlFor="done-true">true</label>
-        <input id="done-true" name="done" type="radio" value="true" onChange={this.handleDone.bind(this)}/>
+        <input id="done-true" name="done" type="radio" value="true" checked={this.state.done === "true"} onChange={this.handleDone.bind(this)}/>
         <label htmlFor="done-false">false</label>
-        <input id="done-false" name="done" type="radio" value="false" onChange={this.handleDone.bind(this)}/>
+        <input id="done-false" name="done" type="radio" value="false" checked={this.state.done === "false"} onChange={this.handleDone.bind(this)}/>
 
         <button>save</button>
       </form>
